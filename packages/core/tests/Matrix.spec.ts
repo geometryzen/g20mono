274b3a7e63e92dfd37c1ce5e28afab790ad8a8ab
David Geo Holmes <geometryzen@gmail.com>
2024-04-25 16:52:17 -0400

    'Matrix computation'

--- packages/core/tests/Matrix.spec.ts
+++ packages/core/tests/Matrix.spec.ts
@@ -40,30 +40,6 @@ describe("Matrix", function () {
         expect(m.a32).toBeCloseTo(h, numDigits);
         expect(m.a33).toBeCloseTo(i, numDigits);
     });
-    it("set_from_matrix", function () {
-        const a = Math.random();
-        const b = Math.random();
-        const c = Math.random();
-        const d = Math.random();
-        const e = Math.random();
-        const f = Math.random();
-        const g = Math.random();
-        const h = Math.random();
-        const i = Math.random();
-        const source = new Matrix();
-        source.set(a, b, c, d, e, f, g, h, i);
-        const m = new Matrix();
-        m.set_from_matrix(source);
-        expect(m.a11).toBeCloseTo(a, numDigits);
-        expect(m.a12).toBeCloseTo(b, numDigits);
-        expect(m.a13).toBeCloseTo(c, numDigits);
-        expect(m.a21).toBeCloseTo(d, numDigits);
-        expect(m.a22).toBeCloseTo(e, numDigits);
-        expect(m.a23).toBeCloseTo(f, numDigits);
-        expect(m.a31).toBeCloseTo(g, numDigits);
-        expect(m.a32).toBeCloseTo(h, numDigits);
-        expect(m.a33).toBeCloseTo(i, numDigits);
-    });
     it("copy", function () {
         const a = Math.random();
         const b = Math.random();
