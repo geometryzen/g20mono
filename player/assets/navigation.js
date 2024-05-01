3020f7a8f644c5f759826de184ef1752d33f0b25
David Geo Holmes <geometryzen@gmail.com>
2024-05-01 09:24:22 -0400

    Updates

--- player/assets/navigation.js
+++ player/assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJyL8pMUdJRKkgsyVCyUkrOSSwuTi3WB4nqZZTk5ijpKGVn5qUoWRkaWdTqoOhyLCkpykwqLUktRujPzCtJLUpLTIYagVCCapiRqVltLAA9snWIhAAAAA=="
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslIKSa0oKS1KVdJRKkgsyVCyUkrOSSwuTi3Wh0roZZTk5ijpKGVn5qUoWRkaWdTGAgBNP62aPQAAAA=="
