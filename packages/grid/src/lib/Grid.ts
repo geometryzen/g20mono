693f84d80a4c8a5171e5a0e466878342864a8c24
David Geo Holmes <geometryzen@gmail.com>
2024-05-08 16:36:55 -0400

    'Axes and Grid'

--- packages/grid/src/lib/Grid.ts
+++ packages/grid/src/lib/Grid.ts
@@ -6,35 +6,33 @@ export interface GridOptions {
 
 export class Grid extends Group {
     constructor(board: Board, options: GridOptions = {}) {
-        super(board, [], options)
-        const Nx = 10        // number of divisions (assumed even)
-        const Ny = 10
-        const Lx = Nx - 1      // number of lines
-        const Ly = Ny - 1      // number of lines
-        const Mx = (Nx / 2) - 1   // middle line
-        const My = (Ny / 2) - 1   // middle line
+        super(board, [], options);
+        const Nx = 10;
+        const Ny = 10;
+        const Lx = Nx - 1;
+        const Ly = Ny - 1;
+        const Mx = (Nx / 2) - 1;
+        const My = (Ny / 2) - 1;
 
-        const bbox = board.getBoundingBox()
+        const bbox = board.getBoundingBox();
 
-        const mx = (bbox.right - bbox.left) / Nx
-        const my = (bbox.top - bbox.bottom) / Ny
+        const mx = (bbox.right - bbox.left) / Nx;
+        const my = (bbox.top - bbox.bottom) / Ny;
 
-        // Horizontal Lines
         for (let i = 0; i < Ly; i++) {
             const line = new Line(board, [bbox.left, my * (i - My)], [bbox.right, my * (i - My)], {
                 strokeWidth: 2 / board.sx,
                 strokeOpacity: 0.5
-            })
-            this.add(line)
+            });
+            this.add(line);
         }
 
-        // Vertical Lines
         for (let i = 0; i < Lx; i++) {
             const line = new Line(board, [mx * (i - Mx), bbox.bottom], [mx * (i - Mx), bbox.top], {
                 strokeWidth: 2 / board.sx,
                 strokeOpacity: 0.5
-            })
-            this.add(line)
+            });
+            this.add(line);
         }
     }
 }
