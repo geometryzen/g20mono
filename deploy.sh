b74ddcfc22d7a0551e743b2c5c62e84f672eff82
David Geo Holmes <geometryzen@gmail.com>
2024-05-15 15:49:49 -0400

    'Lerna Research'

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ read message
 npm install
 npm update
 npm run build
-npm run lint
-npm run test
+#npm run lint
+#npm run test
 git status
 git add --all
 git commit -m "'$message'"
 git push origin main
 npm run version
 npm run publish
-npm run docs
-npm run pages
+#npm run docs
+#npm run pages
