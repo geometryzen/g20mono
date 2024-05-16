0605fcf49879cd4c3ea2a0aca863fdebb47a7d4f
David Geo Holmes <geometryzen@gmail.com>
2024-05-15 22:42:15 -0400

    'Version'

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 echo "Please enter a commit message"
 read message
-npm --registry=http://localhost:4873/ install
-npm --registry=http://localhost:4873/ update
+# npm --registry=http://localhost:4873/ install
+# npm --registry=http://localhost:4873/ update
 npm run build
-#npm run lint
-#npm run test
+npm run lint
+npm run test
 git status
 git add --all
 git commit -m "'$message'"
 git push origin main
 npm run version
 npm run publish
-#npm run docs
-#npm run pages
+npm run docs
+npm run pages
