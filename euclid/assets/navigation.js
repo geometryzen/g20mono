22b165220bffe1b433c003f65beb6e54f7ba033e
David George Holmes <geometryzen@gmail.com>
2024-05-21 21:37:29 -0400

    Updates

--- euclid/assets/navigation.js
+++ euclid/assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJyzixKzkmFkJ55JalFxanJJZn5eUo6SgWJJRlKVkrJOYnFxanF+rhU6mWU5OYo6ShlZ+alKFkZGlnUxgIAY0BYl18AAAA="
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJyzixKzkmFkJ55JalFxanJJZn5eUo6SgWJJRlKVkrJOYnFxanF+rhU6mWU5OYo6ShlZ+alKFkZGlnU6sBNTwarjodSmVjNTyvNA4sU6+NWjWqHmUltLACynL8vwgAAAA=="
